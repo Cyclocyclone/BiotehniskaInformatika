c87d8ffb50fbff3f3596b3f860ce1a0037059e50
Cyclocyclone <78167889+Cyclocyclone@users.noreply.github.com>
2022-11-05 12:58:46 +0100

    Vaja 3

--- Vaja_03/Vaja03.py
+++ Vaja_03/Vaja03.py
@@ -7,22 +7,28 @@ import numpy as np
 
 
 #------------
-path = 'image11.dcm'
+path = 'image01.dcm'
 dcmData = np.array(loadDicomFile(path))
 #-----------
 # image date
 
 dcmImageDate = getDicomDataElement(dcmData, '0008,0023')
-dcmImageStudyTime = getDicomDataElement(dcmData, '0008,0030')
+dcmImageTime = getDicomDataElement(dcmData, '0008,0033')
 dcmImagePatientName = getDicomDataElement(dcmData, '0010,0010')
+dcmImagePatientsAge = getDicomDataElement(dcmData, '0010,1010')
 dcmImageRows = getDicomDataElement(dcmData, '0028,0010')
 dcmImageWindowWidth = getDicomDataElement(dcmData, '0028,1051')
 
+dcmImageStudyTime = getDicomDataElement(dcmData, '0008,0030')
+
 
 print(dcmImageDate)
-print(dcmImageStudyTime)
+print(dcmImageTime)
 print(dcmImagePatientName)
+print(dcmImagePatientsAge)
 print(dcmImageRows)
 print(dcmImageWindowWidth)
+
+print(dcmImageStudyTime)
 
 displayDicomImage(dcmData,path)
