103b549eecc3042ec3a7612194ebf56feb4cdb37
Cyclocyclone <78167889+Cyclocyclone@users.noreply.github.com>
2022-11-19 19:40:15 +0100

    Porocilo

--- Vaja_05/Vaja05.py
+++ Vaja_05/Vaja05.py
@@ -9,11 +9,13 @@ I0 = 10
 #-----------
 #1 SIR, osnovni primer
 t1 = 0
-t2 = 150
+t2 = 150    
 dt = 1
-beta = 1/2
-gamma = 1/3
+beta = 1/3
+gamma = 1/6
 
 SIR, D = solveModelSIR([t1, t2, dt],[N-I0,I0,0],[beta, gamma])
 displayModel(SIR)
+
+print(D)
 
