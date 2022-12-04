fef6dd2f1b0b7c01211709b086c75e6cdfbd4152
Cyclocyclone <78167889+Cyclocyclone@users.noreply.github.com>
2022-12-04 20:59:42 +0100

    Vaja7

--- Vaja_07/Vaja07.py
+++ Vaja_07/Vaja07.py
@@ -15,14 +15,54 @@ img = np.array(img.dataobj)
 img = np.flip(img)
 img = np.transpose(img, [2, 1, 0])
 
-xc = 256
-sagCS = getPlanarCrossSection(img, [1, 0, 0], xc)
-#displayImage(sagCS)
+print('neki')
 
+xc = 60
+#sagCS = getPlanarCrossSection(img, [0, 0, 1], xc)
+#displayImage(sagCS, 'xc = 60')
+
+yc = 35
+#sagCS = getPlanarCrossSection(img, [1, 0, 0], yc)
+#displayImage(sagCS, 'yc = 35')
+
+zc = 90
+#sagCS = getPlanarCrossSection(img, [0, 1, 0], zc)
+#displayImage(sagCS, 'zc = 90')
 #--------
 
-fun = 'mean'
-sagP = getPlanarProjection(img, [0, 0, 1], fun)
-displayImage(sagP)
+#fun = 'max'
+#sagP = getPlanarProjection(img, [1, 1, 0], fun)
+#displayImage(sagP, '[9.24, 3.83, 0]')
+
+#sagP = getPlanarProjection(img, [0, 1, 0], fun)
+#displayImage(sagP, '010 max')
+
+#sagP = getPlanarProjection(img, [1, 0, 0], fun)
+#displayImage(sagP, '100 max')
+
+#sagP = getPlanarProjection(img, [0, 0, 1], fun)
+#displayImage(sagP, '001 max')
+
+#fun = 'mean'
+
+#sagP = getPlanarProjection(img, [0, 1, 0], fun)
+#displayImage(sagP, '010 mean')
+
+#sagP = getPlanarProjection(img, [1, 0, 0], fun)
+#displayImage(sagP, '100 mean')
+
+#sagP = getPlanarProjection(img, [0, 0, 1], fun)
+#displayImage(sagP, '001 mean')
 
 
+fun = 'max'
+
+sagP = getPlanarProjection(img, [3.83, 9.24, 0], fun)
+print('neki2')
+displayImage(sagP, '[3.83, 9.24, 0] max')
+
+sagP = getPlanarProjection(img, [1, 1, 0], fun)
+displayImage(sagP, '[1, 1, 0] max')
+
+sagP = getPlanarProjection(img, [9.24, 3.83, 0], fun)
+displayImage(sagP, '[9.24, 3.83, 0] max')
--- Vaja_07/getPlanarProjection.py
+++ Vaja_07/getPlanarProjection.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 def getPlanarProjection(iImage, iNormVec, iFun):
     oP = []
@@ -15,6 +16,28 @@ def getPlanarProjection(iImage, iNormVec, iFun):
         oP = eval('np.' + iFun + '(iImage, axis=0)')
     elif iNormVec == [0, 0, 1]:
         oP = eval('np.' + iFun + '(iImage, axis=2)')
+
+    elif iNormVec[2] == 0:
+        oP = np.zeros([X,Z])
+        fi = math.atan(iNormVec[0]/iNormVec[1])
+
+        for z in range(Z):
+            toP = np.zeros([Y,X])
+            
+            for y in range(Y):
+                for x in range(X):
+                    x1 = x - X/2
+                    y1 = y - Y/2
+                    xt = round(math.cos(fi)*x1 - math.sin(fi)*y1 + X/2)
+                    yt = round(math.sin(fi)*x1 + math.cos(fi)*y1 + X/2)
+
+                    if((yt<Y) and (yt>0) and (xt<X) and (xt>0)):
+                        toP[y,x] = iImage[yt, xt, z]
+            
+            for x in range(X):
+                oP[x,z] = eval('np.' + iFun + '(toP[:, x])')
+
+        oP = oP
 
 
     return oP
